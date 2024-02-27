2e78cdb050b810eb64b489aaeab3d074598e7531
Wes Dean <wdean@flexion.us>
2024-02-27 14:50:40 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# upload-sarif-to-defectdojo
-Tooling to upload SARIF results to DefectDojo via CLI or CI/CD
+# Upload Sarif results to Defect Dojo
+
+## Configuration Values
+
+### DD_TOKEN
+
+`DD_TOKEN` is authentication token for interacting with DefectDojo (required)
+
+The API token may be found throught DefectDojo's web user interface
+by going to `<server name>/api/key-v2`
+
+### DD_PRODUCT
+
+`DD_PRODCT` is name of the product in DefectDojo (required)
+
+### DD_ENGAGEMENT
+
+`DD_ENGAGEMENT` is name of the engagement in DefectDojo
+
+The default value is "cicd" (lowercase, no slash).
+
+### DD_SERVER_PROTO
+
+`DD_SERVER_PROTO` is the protocol / scheme to use when talking to DefectDojo
+
+The default value is `https`.
+
+### DD_SERVER_HOST
+
+`DD_SERVER_HOST` is the hostname of the DefectDojo server (required)
+
+### DD_SERVER_PATH
+
+`SS_SERVER_PATH` is path on the server to the import-scan API endpoint
+
+The default is `/api/v2/import-scan/` which is the standard when
+DefectDojo runs at the root of the server (i.e., `dojo.example.com`)
+
+### DD_SCAN_DATE
+
+`DD_SCAN_DATE` the date the scan took place
+
+DefectDojo accepts ISO-8601 dates (but just year, month, and day)
+for when scans took place; the default value is when the file being
+uploaded was last modified
+
+### DD_MINIMUM_SEVERITY
+
+`DD_MINIMUM_SEVERITY` IS minimum severity level to be imported
+
+The default value is 'Info'; values may be:
+
+* Info
+* Low
+* Medium
+* High
+* Critica
+
+### DD_ACTIVE
+
+`DD_ACTIVE` specifies whether or not the findings are active
+
+the default value is 'true'
+
+### DD_VERIFIED
+
+`DD_VERIFIED` specifies whether or not a finding has been verified
+
+The default value is 'true'
+
+### DD_SCAN_TYPE
+
+`DD_SCAN_TYPE` is the type of scan results to be imported
+
+The default value is determined by the file's extension
+
+### DD_CLOSE_OLD_FINDINGS
+
+`DD_CLOSE_OLD_FINDINGS` is to close old findings as mitigated when importing
+
+The default value is 'false'
+
+### DD_CLOSE_OLD_FINDINGS_PRODUCT_SCOPE
+
+`DD_CLOSE_OLD_FINDINGS_PRODUCT_SCOPE` will restrict closing to this product
+
+The default value is 'false'
+
+### DD_PUSH_TO_JIRA
+
+`DD_PUSH_TO_JIRA` is whether or not to push findings to Jira as well
+
+The default value is 'false'
+
+### DD_CREATE_FINDINGS_GROUP
+
+`DD_CREATE_FINDINGS_GROUP` will create finding groups for single findings
+
+If false, findings will be grouped only when multiple findings.  The default
+value is 'true'.  Regardless of this setting, scans with multiple findings
+will always be grouped
+
+### DD_FILE_TYPE
+
+`DD_FILE_TYPE` is the MIME type for the file to be uploaded
+
+The default value is determined by the file's extension
+
+### DD_BRANCH
+
+`DD_BRANCH` is the SCM branch where the finding was applicable
+
+This is an optional field with no default
---
+++ upload_sarif_to_defectdojo.bash
@@ -0,0 +1,288 @@
+#!/usr/bin/env bash
+
+## @file upload_sarif_to_defectdojo.bash
+## @author CQPFC Team
+## @brief a shell script to automate uploading SARIF results to DefectDojo
+## @details
+## This is a shell script that will iterate across a series of filenames
+## passed in and upload the results to a DefectDojo instance.  This
+## hope is to have one process generate SARIF results (e.g., Megalinter)
+## so that this script can upload the results.
+##
+## There exist actions in the GitHub Actions Marketplace that will
+## upload SARIF results to DefectDojo, such as:
+## https://github.com/marketplace/actions/defectdojo-import-scan
+##
+## However, we want to be able to be able to upload results to
+## an internal, non-Internet-accessible DefectDojo instance, potentially
+## using an internal CI/CD system (e.g., a Jenkins instance).
+##
+## Configuration for the tool is expected to be provided by environment
+## variables; this is to support clean integration with a CI/CD
+## system that populates environment variables rather than using
+## flags.  Additionally, the tool is able to use a configuration
+## file (e.g., `.env`) that can provide values.
+##
+## The expected usage pattern is for a repository to include a
+## configuration file with parameters like project name, whether
+## or not to push results to Jira, etc. and environment variables to
+## pass server details and authentication credentials.  It's possible
+## to use all environment variables or all configuration files or
+## some mix.
+##
+## The script supports passing multiple files to be uploaded, even
+## if those files are in different locations or even associated with
+## different projects. In situations like these, a configuration
+## file for each location is supported.
+##
+## Several locations for configuration files are searched with the
+## first one found being used:
+##
+## 1. current directory's uploadsarifdd.conf
+## 2. current directory's .uploadsarifdd.conf
+## 3. file's repo's uploadsarifdd.conf
+## 4. file's repo's .uploadsarif.dd.conf
+## 5. ~/uploadsarifdd.conf
+## 6. ~/.uploadsarifdd.conf
+##
+## Future plans may include specifying the configuration via
+## CLI flag, supporting additional scan types, and/or additional
+## fields from DefectDojo's import-scan endpoint.
+## 
+## @par Examples
+## @code
+## upload_sarif_to_defectdojo.bash megalinter-reports/sarif/*.sarif
+## @endcode
+
+
+set -euo pipefail
+
+
+## @fn is_git_repository()
+## @brief determine if a file is in a git-associated file structure
+## @details
+## This determines in the directory structure where a file is located
+## is associated with a git repository.  It does NOT check to see if
+## a file is staged or tracked by git.  We want to be able to have
+## a tool (e.g., Megalinter) deposit scan results (e.g., to
+## megalinter-reports/sarif/REPOSITORY_KICS.sarf without having
+## to add that file to the repository.  This will allow us to check
+## to see if there's a configuration file in the root of the repository
+## if, in fact, it's being run from inside of a git repository.
+## @param filename the filename to use as a basis for searching
+## @retval 0 (True) if the file's in a git directory
+## @retval 1 (False) if the file isn't in a git directory
+## @par Examples
+## @code
+## if is_git_repository "megalinter-reports/sarif/REPOSITORY_KICS.sarif" ; then
+## @endcode
+is_git_repository() {
+  directory="$(dirname "${1:-.}")"
+
+  (
+    cd "$directory" || exit 1
+    git rev-parse --quiet > /dev/null 2>&1
+  )
+}
+
+
+## @fn git_branch()
+## @brief determine the current branch of a git repository
+## @details
+## This will determine the current branch of a git repository
+## where a specified file lives.  Unlike `git rev-parse`,
+## we can't provide a `--prefix` so we're just going to
+## `cd` there and run `git branch`.
+## @param filename the file to use as a basis for searching
+## @retval 0 (True) if a branch could be determined
+## @retval 1 (False) if a branch could not be determined
+## @par Examples
+## @code
+## filename=~/src/projecta
+## echo "The current branch for '$filename' is '$(git_branch "$filename")'"
+## @endcode
+git_branch() {
+  directory="$(dirname "${1:-.}")"
+
+  (
+    cd "$directory" || exit 1
+    git branch --show-current
+  )
+}
+
+
+## @fn get_scan_type()
+## @brief determine the scan type based on a filename
+## @details
+## DefectDojo doesn't get the filename we're sending, so we
+## have to explicitly tell it what type of scan results we're
+## sending to it.  This looks at the filename and attempts to
+## determine what we're sending based on the filename.  So,
+## for example, if a file matches *.sarif, we tell DefectDojo
+## that we're sending SARIF results.  Note, this really only
+## looks at the filename -- it doesn't interact with the file;
+## in fact, it doesn't matter if the file exists or not.
+## @param filename the filename to examine
+## @retval 0 (True) if a scan type was determined
+## @retval 1 (False) if a scan type could not be determined
+## @par Examples
+## @code
+## echo "The scan type was $(get_scan_type "foobar.sarif")"
+## @endcode
+get_scan_type() {
+  case "${1?No filename provided to get_scan_type}" in
+    *.sarif)
+      echo "SARIF"
+      ;;
+    *)
+      echo "Unable to determine scan type" 1>&2
+      exit 1
+      ;;
+  esac
+}
+
+
+## @fn get_mime_type()
+## @brief determine a file's MIME type based on a filename
+## @details
+## Just like get_scan_type(), this looks at a filename (which
+## may not exist) and attempts to determine its MIME type.
+## As a fallback, we use the `file` command to try to figure
+## it out.  If that fails, we're done.  It's less likely that
+## `file` will optimally detect the type; for example, a SARIF
+## file is reported as "text/plain" rather than ## "application/sarif"
+## Also, we're doing the simple test first because want to
+## minimize the number of external dependencies
+## @param filename the filename to examine
+## @retval 0 (True) if a MIME type could be guessed
+## @retval 1 (False) if a MIME type couldn't be determined
+## @par Examples
+## @code
+## curl -F "file=@filename;type=$(get_mime_type "$filename")" ...
+## @endcode
+get_mime_type() {
+  case "${1?No filename provded to get_mime_type}" in
+    *.sarif)
+      echo "application/SARIF"
+      ;;
+    *)
+      file --brief --mime-type "$1"
+      ;;
+  esac
+}
+
+
+## @fn get_scan_date()
+## @brief determine when a scan report was updated
+## @details
+## This will look at a provided filename, extract its last modification
+## date, and then returns the year, month, and day-of-month in ISO-8601
+## format (YYYY-mm-dd); DefectDojo only accepts the date, not the full
+## ISO-8601 formatted datetime string.  The result is returned via STDOUT.
+## @param filename the filename to examine
+## @param DD_SCAN_DATE force this specific date
+## @retval 0 (True) if a date could be determined
+## @retval 1 (False) if a date could not be determined
+## @par Examples
+## @code
+## date1="$(get_scan_date "$filename1")"
+## date2="$(DD_SCAN_DATE=2024-02-26 get_scan_date "$filename2")"
+## DD_SCAN_DATE=2024-02-27
+## get_scan_date "$filename3"
+## @endcode
+get_scan_date() {
+  filename"${1?No filename provided to get_scan_date}"
+  scan_date="${DD_SCAN_DATE:-$(date +'%Y-%m-%d' -d "$(stat -L -c '%y' "$filename")")}"
+}
+
+
+declare -a configuration_sources
+declare -a form_values
+
+if [ "$#" -eq 0 ] ; then
+  echo "Error: no report filenames passed" 1>&2
+  exit 1
+fi
+
+
+for filename in "$@" ; do
+ form_values=()
+
+  configuration_sources=(
+  "./uploadsarifdd.conf"
+  "./.uploadsarifdd.conf"
+  )
+
+  if is_git_repository "$filename" ; then
+    configuration_sources+=("$(git rev-parse --show-toplevel --prefix "$filename")/uploadsarifdd.conf")
+    configuration_sources+=("$(git rev-parse --show-toplevel --prefix "$filename")/.uploadsarifdd.conf")
+  fi
+
+  configuration_sources+=("${HOME}/uploadsarifdd.conf")
+  configuration_sources+=("${HOME}/.uploadsarifdd.conf")
+
+  for configuration_file in "${configuration_sources[@]}" ; do
+    if [ -e "$configuration_file" ] ; then
+      echo "Importing configuration from $configuration_file"
+
+      set -o allexport
+      # shellcheck disable=SC1090
+      source "$configuration_file"
+      set +o allexport
+
+      break
+    fi
+  done
+
+  if [ -z "$DD_TOKEN" ] ; then
+    echo "No value for DD_TOKEN provided" 1>&2
+    exit 1
+  fi
+
+  if [ -z "$DD_PRODUCT" ] ; then
+    echo "No value for DD_PRODUCT provided" 1>&2
+    exit 1
+  fi
+
+  if [ -z "$DD_SERVER_HOST" ] ; then
+    echo "No value for DD_SERVER_HOST provided" 1>&1
+    exit 1
+  fi
+
+  scan_date="${DD_SCAN_DATE:-$(date +'%Y-%m-%d' -d "$(stat -L -c '%y' "$filename")")}"
+
+  form_values+=("active=${DD_ACTIVE:-true}")
+
+  form_values+=("minimum_severity=${DD_MINIMUM_SEVERITY:-Info}")
+
+  form_values+=("verified=${DD_VERIFIED:-true}")
+
+  form_values+=("scan_type=${DD_SCAN_TYPE:-$(get_scan_type "$filename")}")
+
+  form_values+=("product_name=${DD_PRODUCT?No DD_PRODUCT provided}")
+
+  form_values+=("engagement_name=${DD_ENGAGEMENT:-cicd}")
+
+  form_values+=("close_old_findings=${DD_CLOSE_OLD_FINDINGS:-false}")
+
+  form_values+=("close_old_findings_product_scope=${DD_CLOSE_OLD_FINDINGS_PRODUCT_SCOPE:-false}")
+
+  form_values+=("push_to_jira=${DD_PUSH_TO_JIRA:-false}")
+
+  form_values+=("create_finding_groups_for_all_findings=${DD_CREATE_FINDINGS_GROUP:-false}")
+
+  form_values+=("scan_date=${scan_date}")
+  form_values+=("file=@${filename};type=${DD_FILE_TYPE:-$(get_mime_type "$filename")}")
+
+  if is_git_repository "$filename" \
+  || [ -n "${DD_BRANCH:-}" ] ; then
+    form_values+=("branch=${DD_BRANCH:-$(git_branch "$filename")}")
+  fi
+
+  echo curl -X 'POST' \
+    "${DD_SERVER_PROTO:-https}://${DD_SERVER_HOST}${DD_SERVER_PATH:-/api/v2/import-scan/}" \
+    -H "accept: application/json" \
+    -H "Content-Type: multipart/form-data" \
+    -H "Authorization: Token ${DD_TOKEN}" \
+    "${form_values[@]/#/-F }"
+done
